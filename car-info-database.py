3439459260fb75a6e6b8041f0d8f4fe14a63946d
sarahliu2006 <mail2sarahl@gmail.com>
2023-01-22 15:34:37 -0800

    wha

--- car-info-database.py
+++ car-info-database.py
@@ -1,6 +1,7 @@
 import json
 import os
 import random
+db = SQLAlchemy(app) 
 
 # I'm still setting up and changing things, there is a TODO below, most important is assigning and comparing
 
@@ -30,4 +31,20 @@ class CarInfo:
         return self.car_info[name]
 
     # TODO: make this work ,Here is where I'm trying to get the info of users assigned
-    
+    def User(db.Model):
+    # Define the User schema with "vars" from object
+    id = db.Column(db.Integer, primary_key=True)
+    _name = db.Column(db.String(255), unique=False, nullable=False)
+    _uid = db.Column(db.String(255), unique=True, nullable=False)
+    _password = db.Column(db.String(255), unique=False, nullable=False)
+    _dob = db.Column(db.Date)
+
+    # Defines a relationship between User record and Notes table, one-to-many (one user to many notes)
+    posts = db.relationship("Post", cascade='all, delete', backref='users', lazy=True)
+
+    # constructor of a User object, initializes the instance variables within object (self)
+    def __init__(self, name, uid, password="123qwerty", dob=date.today()):
+        self._name = name    # variables with self prefix become part of the object, 
+        self._uid = uid
+        self.set_password(password)
+        self._dob = dob
